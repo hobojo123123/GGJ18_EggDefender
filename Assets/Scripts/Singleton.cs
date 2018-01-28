46653ec5b8103b278c6ffcb90fabed702f04f945
George Wulfers <george.wulfers@gmail.com>
2018-01-27 18:09:15 -0800

    Adding my code for the sound stuff right meow

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,34 @@
+﻿/*
+     File Name: Singleton.cs
+     Date : 01/27/2018
+     Owner: George Wulfers
+     ----------------------------------
+     This is the base class for the Singleton Design patterns
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    private static Singleton Instance;
+    public static Singleton instance
+    {
+        get
+        {
+            if (Instance == null)
+            {
+                Instance = new Singleton();
+            }
+            return Instance;
+        }
+    }
+
+    protected Singleton()
+    {
+
+    }
+
+    
+}
