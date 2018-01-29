a1b5b29a6c830b3325aaf127cb4259f8509bd302
DESKTOP-AD9EJR4\Gavin Delima <gdelima91@gmail.com>
2018-01-28 16:59:40 -0800

    last submit

--- Assets/Scripts/Weapon_FullAutoNoReloadBasic.cs
+++ Assets/Scripts/Weapon_FullAutoNoReloadBasic.cs
@@ -4,45 +4,7 @@ using UnityEngine;
 
 public class Weapon_FullAutoNoReloadBasic : WeaponSystemBase {
 
-    float shotTimer;
 
-    public GameObject projectile;
-    public Transform firePoint;
-
-	// Use this for initialization
-	void Start () {
-        //fireRate = 1;
-        shotTimer = fireRate;
-	}
-	
-	// Update is called once per frame
-	protected virtual void Update () {
-        ClickToShoot();
-        InputManager();
-        print(firePoint);
-	}
-
-    void ClickToShoot () {
-
-        if (shotTimer < fireRate) {
-            shotTimer += Time.deltaTime;
-        } else {
-            bCanShoot = true;
-        }
-        if (bWantsToShoot && bCanShoot) {
-            shotTimer = 0;
-            Shoot();
-        }
-    }
-
-    void Shoot () {
-        bCanShoot = false;
-        if (firePoint != null) {
-            Instantiate(projectile, firePoint.position, firePoint.rotation);
-        } else {
-            Instantiate(projectile, transform.position, transform.rotation);
-        }
-    }
 
     
 }
